447ec8f89e9deda83918abe090845ebf33ea5b41
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 17:34:09 -0300

    feat(SubmitFormButton): configure the formulary button

--- src/components/home/info/form/index.tsx
+++ src/components/home/info/form/index.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { CitiesSelect } from "./cities-select";
 import { StatesSelect } from "./states-select";
+import { SubmitFormButton } from "./submit-form-button";
 
 export const Form = () => {
   const [selectedState, setSelectedState] = useState<number | undefined>(12); // 12 = Acre's pip
 
   return (
-    <form className="flex flex-col gap-2 w-full md:w-fit md:flex-row md:items-center">
-      <p className="text-extra-small-size md:text-nowrap md:text-small-size">Search a friend:</p>
-      <label className="flex gap-2 w-full">
+    <form className="flex w-full flex-col gap-2 md:w-fit md:flex-row md:items-center">
+      <p className="text-extra-small-size md:text-small-size md:text-nowrap">
+        Search a friend:
+      </p>
+      <label className="flex w-full gap-2">
         <StatesSelect setSelectedState={setSelectedState} />
         <CitiesSelect selectedState={selectedState} />
       </label>
+      <SubmitFormButton />
     </form>
   );
 };
---
+++ src/components/home/info/form/submit-form-button/index.tsx
@@ -0,0 +1,14 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const SubmitFormButton = () => (
+  <button
+    className="bg-tertiary-color group hover:ring-tertiary-color hover:bg-secondary-color flex h-12 cursor-pointer items-center justify-center rounded-lg duration-300 hover:ring md:hover:ring-2"
+    type="submit"
+  >
+    <FontAwesomeIcon
+      icon={faMagnifyingGlass}
+      className="text-secondary-color group-hover:text-tertiary-color duration-300 group-hover:scale-125 group-hover:drop-shadow-lg"
+    />
+  </button>
+);
