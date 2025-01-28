d2686123aaec10ddc4b4516d93d401b6073b2b08
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 18:07:57 -0300

    style(Home): adjust layout to improve section spacing and alignment

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,7 +3,7 @@ import { Hero } from "./hero";
 import { Info } from "./info";
 
 export const Home = () => (
-  <section className="bg-primary-color text-base-color flex min-h-screen w-full flex-col items-center md:justify-evenly md:gap-10">
+  <section className="bg-primary-color text-base-color flex min-h-screen w-full flex-col items-center justify-between md:justify-evenly md:gap-10">
     <Header />
     <Hero />
     <Info />
