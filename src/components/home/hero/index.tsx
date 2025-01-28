650d1f8fd1dd8e2684f5eefb64d4c73162b77c22
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 14:36:43 -0300

    style(home): adjust text style and select tag size

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -1,7 +1,7 @@
 import heroImage from "@/assets/imgs/hero.webp";
 
 export const Hero = () => (
-  <section className="flex max-w-screen-2xl flex-1 flex-col items-center justify-center px-4 md:flex-row md:justify-center md:px-8">
+  <section className="flex max-w-screen-2xl flex-col items-center justify-center px-4 md:flex-row md:justify-center md:px-8">
     <h2 className="text-base-size md:text-large-size lg:text-extra-large-size w-1/2 min-w-56 text-center leading-none font-bold md:flex md:w-2/5 md:text-start">
       Bring happiness to your home
     </h2>
--- src/components/home/info/index.tsx
+++ src/components/home/info/index.tsx
@@ -2,7 +2,7 @@ import { Form } from "./form";
 
 export const Info = () => (
   <footer className="flex w-full max-w-screen-2xl flex-col gap-4 p-8 pt-0 md:flex-row md:items-center md:justify-between md:gap-4">
-    <p className="text-small-size">Find a perfect pet to your lifestyle!</p>
+    <p className="text-small-size md:text-base-size md:w-1/3">Discover your ideal pet companion, perfectly matched to your unique lifestyle!</p>
     <Form />
   </footer>
 );
