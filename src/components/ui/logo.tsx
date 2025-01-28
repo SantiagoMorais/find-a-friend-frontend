463d41e3147245b64562da2dfbd362c9e8aacd27
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 10:31:55 -0300

    feat(Logo): update logo link to use routes and add routes utility

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,12 +1,17 @@
 import logo from "@/assets/imgs/logo.png";
+import { routes } from "@/utils/routes";
 import { Link } from "react-router-dom";
 
 export const Logo = () => (
   <Link
-    to={"/"}
-    className="drop-shadow-sm text-base-color flex items-center gap-2 duration-300 hover:scale-110 hover:drop-shadow-white"
+    to={routes.home}
+    className="text-base-color hover:drop-shadow-white flex items-center gap-2 drop-shadow-sm duration-300 hover:scale-110"
   >
-    <img src={logo} alt="Logo" className="size-10 brightness-0 invert duration-300" />
+    <img
+      src={logo}
+      alt="Logo"
+      className="size-10 brightness-0 invert duration-300"
+    />
     <h1 className="text-small-size font-bold">FindAFriend</h1>
   </Link>
 );
