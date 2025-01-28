62cf8ee7c52026785bfc1a4c1a9941a9d5b80a83
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 19:27:38 -0300

    feat(Header): replace Sign In button with Link component and add Sign Up route

--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -1,5 +1,7 @@
 import { Logo } from "@/components/ui/logo";
 import { button } from "@/styles";
+import { routes } from "@/utils/routes";
+import { Link } from "react-router-dom";
 
 export const Header = () => (
   <header className="flex w-full max-w-screen-2xl flex-col items-center justify-center gap-4 p-5 md:flex-row md:justify-between md:px-10 md:py-8">
@@ -7,7 +9,9 @@ export const Header = () => (
     <nav className="flex flex-col items-center">
       <h2 className="md:text-small-size font-bold">For Organizations:</h2>
       <div className="flex justify-between gap-2">
-        <button className={button({ color: "primary" })}>Sign In</button>
+        <Link to={routes.signIn} className={button({ color: "primary" })}>
+          Sign In
+        </Link>
         <button className={button({ color: "primary" })}>Sign Up</button>
       </div>
     </nav>
