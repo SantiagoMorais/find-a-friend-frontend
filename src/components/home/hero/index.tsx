4125cda0317ee6c3d1a3b9e1b7e6edf7f98cbb12
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 11:37:02 -0300

    feat(Hero): configure hero component

---
+++ src/components/home/hero/index.tsx
@@ -0,0 +1,12 @@
+import heroImage from "@/assets/imgs/hero.webp";
+
+export const Hero = () => (
+  <section className="flex max-w-screen-2xl flex-1 flex-col items-center px-4 md:flex-row md:justify-center md:px-8">
+    <h2 className="text-base-size md:text-large-size lg:text-extra-large-size w-1/2 min-w-56 text-center leading-none font-bold md:flex md:w-2/5 md:text-start">
+      Bring happiness to your home
+    </h2>
+    <figure className="md:max w-full md:flex-1">
+      <img src={heroImage} alt="hero image" className="w-full" />
+    </figure>
+  </section>
+);
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,7 +1,9 @@
 import { Header } from "./header";
+import { Hero } from "./hero";
 
 export const Home = () => (
   <section className="bg-primary-color text-base-color flex min-h-screen w-full flex-col items-center">
     <Header />
+    <Hero />
   </section>
 );
