865f89b0ea0ecd07150d61b6f5842fd13ea35bae
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 14:02:42 -0300

    style(App): add overflow-x-hidden to prevent horizontal scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { AppRoutes } from "pages/app-routes";
 
 export const App = () => (
-  <section className="flex min-h-screen w-full justify-center">
+  <section className="flex min-h-screen w-full justify-center overflow-x-hidden">
     <AppRoutes />
   </section>
 );
