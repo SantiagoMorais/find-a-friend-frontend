15c64e91815de0905a79465180c8c75cc5e3dbb4
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-28 10:30:38 -0300

    style(Logo): added hover effect to logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,8 +1,12 @@
 import logo from "@/assets/imgs/logo.png";
+import { Link } from "react-router-dom";
 
 export const Logo = () => (
-  <figure className="flex items-center gap-2 text-base-color">
-    <img src={logo} alt="Logo" className="size-10 invert brightness-0"/>
-    <h1 className="font-bold text-small-size">FindAFriend</h1>
-  </figure>
+  <Link
+    to={"/"}
+    className="drop-shadow-sm text-base-color flex items-center gap-2 duration-300 hover:scale-110 hover:drop-shadow-white"
+  >
+    <img src={logo} alt="Logo" className="size-10 brightness-0 invert duration-300" />
+    <h1 className="text-small-size font-bold">FindAFriend</h1>
+  </Link>
 );
