d78de3bf65243fe87421c2e30d4e5d43d2cc1364
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-04 17:38:56 -0300

    feat(NotLoggedButtons): configure the organization name and buttons on navbar

--- src/components/home/header/not-logged-buttons/index.tsx
+++ src/components/home/header/not-logged-buttons/index.tsx
@@ -1,11 +1,30 @@
 import { SignOutButton } from "@/components/ui/sign-out-button";
+import { THandleOrganizationProfile } from "@/core/types/handle-organization-profile";
+import { handleOrganizationProfile } from "@/functions/handle-organization-profile";
+import { button } from "@/styles";
+import { useEffect, useState } from "react";
 
 export const NotLoggedButtons = () => {
-  // collect organization data logic
+  const [orgData, setOrgData] = useState<THandleOrganizationProfile | null>(
+    null
+  );
+
+  useEffect(() => {
+    const handleGetOrgData = async () => {
+      const organizationResponse = await handleOrganizationProfile();
+      if (organizationResponse) setOrgData(organizationResponse);
+    };
+
+    handleGetOrgData();
+  }, [orgData]);
+
   return (
     <>
-      <h2 className="md:text-small-size font-bold">lorem ipsum</h2>
+      <h2 className="md:text-small-size font-bold">
+        Welcome, {orgData?.organization.name}!
+      </h2>
       <div className="flex justify-between gap-2">
+        <button className={button({ color: "primary" })}>Register pets</button>
         <SignOutButton />
       </div>
     </>
--- src/functions/handle-organization-profile.ts
+++ src/functions/handle-organization-profile.ts
@@ -3,17 +3,18 @@ import { env } from "@/env";
 import { productionAmbience } from "@/utils/variables";
 import axios from "axios";
 
-export const handleOrganizationProfile = async () => {
-  await axios
-    .get<THandleOrganizationProfile>(
-      `${env.VITE_DATABASE_URL}/organizations/profile`
-    )
-    .then((response) => {
-      return response;
-    })
-    .catch((err) => {
-      if (!productionAmbience)
-        console.log("Error getting organization profile:", err);
-      return null;
-    });
-};
+export const handleOrganizationProfile =
+  async (): Promise<THandleOrganizationProfile | null> => {
+    return await axios
+      .get<THandleOrganizationProfile>(
+        `${env.VITE_DATABASE_URL}/organizations/profile`
+      )
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        if (!productionAmbience)
+          console.log("Error getting organization profile:", err);
+        return null;
+      });
+  };
