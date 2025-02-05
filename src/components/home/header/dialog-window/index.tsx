2d35fafe504c8e0b4159d97d62200c15fa913619
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-05 18:26:01 -0300

    feat(dialog-window): configure component

---
+++ src/components/home/header/dialog-window/index.tsx
@@ -0,0 +1,59 @@
+import { useAuth } from "@/contexts/auth-context";
+import { TDialogWindow } from "@/core/types/components/dialog-window";
+import { handleLogout } from "@/functions/handle-log-out";
+import { button } from "@/styles";
+import { routes } from "@/utils/routes";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const DialogWindow = ({ openModal, setOpenModal }: TDialogWindow) => {
+  const { setToken } = useAuth();
+  const route = useNavigate();
+
+  const handleLogoutSession = async () => {
+    await handleLogout(setToken);
+    route(routes.home);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") setOpenModal(false);
+    };
+
+    if (openModal) {
+      document.addEventListener("keydown", handleKeyDown);
+    } else {
+      document.removeEventListener("keydown", handleKeyDown);
+    }
+
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [openModal]);
+
+  return (
+    <dialog open={openModal} className="top-0 bottom-0 mx-auto my-auto">
+      <div
+        onClick={() => setOpenModal(false)}
+        className="fixed top-0 left-0 z-0 min-h-screen min-w-screen bg-black/60 backdrop-blur-xs"
+      ></div>
+      <div className="bg-tertiary-color ring-secondary-color relative z-10 flex flex-col items-center gap-4 rounded-lg p-5 ring-2 md:p-8">
+        <h2 className="text-small-size md:text-base-size text-secondary-color w-full text-center">
+          Are you sure that you want to logout?
+        </h2>
+        <div className="flex w-full flex-col gap-4 md:flex-row">
+          <button
+            onClick={handleLogoutSession}
+            className={button({ color: "secondary", className: "py-2" })}
+          >
+            Yes! 👍
+          </button>
+          <button
+            onClick={() => setOpenModal(false)}
+            className={button({ color: "secondary", className: "py-2" })}
+          >
+            Cancel! 🙅‍♂️
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
+};
--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -2,16 +2,24 @@ import { Logo } from "@/components/ui/logo";
 import { useAuth } from "@/contexts/auth-context";
 import { LoggedButtons } from "./logged-buttons";
 import { NotLoggedButtons } from "./not-logged-buttons";
+import { useState } from "react";
+import { DialogWindow } from "./dialog-window";
 
 export const Header = () => {
   const { token } = useAuth();
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
   return (
     <header className="flex w-full max-w-screen-2xl flex-col items-center justify-center gap-4 p-5 md:flex-row md:justify-between md:px-10 md:py-8">
       <Logo />
       <nav className="flex flex-col items-center">
-        {!token ? <LoggedButtons /> : <NotLoggedButtons />}
+        {!token ? (
+          <LoggedButtons />
+        ) : (
+          <NotLoggedButtons openModal={openModal} setOpenModal={setOpenModal} />
+        )}
       </nav>
+      <DialogWindow openModal={openModal} setOpenModal={setOpenModal} />
     </header>
   );
 };
